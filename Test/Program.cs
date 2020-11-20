abf65b1c923445e1d1d83304872b2e753bde44b0
yjy <yjy@ekuaibao.com>
2020-11-20 15:59:25 +0800

    更新

--- Test/Program.cs
+++ Test/Program.cs
@@ -16,76 +16,20 @@ namespace ConsoleApp2
     {
         static async Task Main(string[] args)
         {
-
-
-            string sConfigurationFile = "d:\\test.json";
-            Console.Write($"Load external configuration file: {sConfigurationFile}");
             var _mConfig = new ConfigurationBuilder();
-            _mConfig.AddJsonFile(sConfigurationFile);
             var xxx = _mConfig.Build();
-
-            // Create a service container with Elsa services.
             var services = new ServiceCollection()
                 .AddTransient<IDbContext, DbContext>()
                 .AddTransient<ISerializer, JsonSerializer>()
                 .AddTransient<IConnectionProvider, MySql>()
-               .AddSingleton<IConfiguration>(xxx);
-
-
-            List<string> aDir = new List<string>();
-            string ZERO_ADDONS_DIR = string.Empty;
-
-         //   NLogger.Info("Plugin Location(ZERO_ADDONS_DIR) = [" + ZERO_ADDONS_DIR + "]");
-
-            if (!string.IsNullOrEmpty(ZERO_ADDONS_DIR))
-            {
-                ZERO_ADDONS_DIR = ZERO_ADDONS_DIR + ";";
-
-                foreach (string dd2 in ZERO_ADDONS_DIR.Split(new char[1] { ';' }))
-                {
-                    if (!string.IsNullOrEmpty(dd2) && Directory.Exists(dd2))
-                    {
-                        aDir.Add(dd2);
-                    }
-                }
-            }
-
-            string sAddonsDirectory = Directory.GetParent(Directory.GetCurrentDirectory()) + Path.DirectorySeparatorChar.ToString() + "addons";
-            //NLogger.Info("AddonsDirectory = " + sAddonsDirectory);
-
-            if (Directory.Exists(sAddonsDirectory) && aDir.IndexOf(sAddonsDirectory) < 0)
-            {
-                aDir.Add(sAddonsDirectory);
-            }
-
-            string sBinDirectory = Directory.GetCurrentDirectory();
-            if (Directory.Exists(sBinDirectory))
-            {
-                aDir.Add(sBinDirectory);
-            }
-
-            StringBuilder ScanClass = new StringBuilder();
-            ScanClass.AppendLine();
-            //services.Scan(scan =>
-            //        scan.FromAssemblies(_Assembly)
-            //        .AddClasses(classes =>
-            //            classes.Where(t => {
-            //                ScanClass.AppendLine(t.Name);
-            //                return true;
-            //            })
-            //            )
-            //        .AsSelf()
-            //        .AsImplementedInterfaces()
-            //        .WithTransientLifetime());
-
-            Console.WriteLine(ScanClass.ToString());
+                .AddSingleton<IConfiguration>(xxx);
 
             var svc = services.BuildServiceProvider();
 
             // Run startup actions (not needed when registering Elsa with a Host).
             var startupRunner = svc.GetRequiredService<IDbContext>();
             startupRunner.Open("master");
-            startupRunner.BeginTransaction();
+            //startupRunner.BeginTransaction();
 
             for (int i = 0; i < 10; i++)
             {
@@ -93,9 +37,9 @@ namespace ConsoleApp2
                 startupRunner.AddNewEntity<Person>(vPerson);
             }
 
-            startupRunner.Writeable = true;
-            startupRunner.Commit();
-
+            //startupRunner.Writeable = true;
+            //startupRunner.Commit();
+            Console.WriteLine("OK,press any key to exit");
             Console.ReadLine();
         }
     }
